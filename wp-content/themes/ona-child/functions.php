2427a04c09581bf8fc42bf7685f3049830d8ba52
Andre <andre.i.jonsson@gritacademy.se>
2025-01-28 19:27:55 +0100

    uppdaterad functions

--- wp-content/themes/ona-child/functions.php
+++ wp-content/themes/ona-child/functions.php
@@ -1,4 +1,13 @@
 <?php
+function my_child_theme_styles() {
+    // Load parent theme styles
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    
+    // Load child theme styles
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+}
+add_action('wp_enqueue_scripts', 'my_child_theme_styles');
+
 function child_theme_button_styles() {
     wp_enqueue_style('child-theme-button-styles', get_stylesheet_directory_uri() . '/btnstyle.css');
 }
